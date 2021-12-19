7ac129d2826fc98d01501a3c49556a45635201d6
Adrian Stevens <adrian.stevens@gmail.com>
2021-12-19 13:27:24 -0800

    Update DeployProvider to check for OS firmware before deploying (#45)
    * Update DeployProvider to check for OS firmware before deploying
    
    * Simplify OS download code in DeployProvider

--- VS_Meadow_Extension/VS_Meadow_Extension.Shared/DeployProvider.cs
+++ VS_Meadow_Extension/VS_Meadow_Extension.Shared/DeployProvider.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
+using Meadow.CLI.Core;
 using Meadow.CLI.Core.DeviceManagement;
 using Meadow.CLI.Core.Devices;
 using Meadow.Helpers;
@@ -79,6 +80,20 @@ namespace Meadow
                 }
 
                 Meadow = new MeadowDeviceHelper(device, logger);
+
+                //wrap this is a try/catch so it doesn't crash if the developer is offline
+                try
+                {
+                    string osVersion = await Meadow.GetOSVersion(TimeSpan.FromSeconds(30), token)
+                        .ConfigureAwait(false);
+
+                    await new DownloadManager(logger).DownloadLatestAsync(osVersion)
+                        .ConfigureAwait(false);
+                }
+                catch
+                {
+                    logger.Log("OS download failed, make sure you have an active internet connection");
+                }
 
                 var appPathDll = Path.Combine(folder, "App.dll");
 
