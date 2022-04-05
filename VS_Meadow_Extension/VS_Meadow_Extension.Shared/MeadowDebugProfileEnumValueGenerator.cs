4cef771a035de983f3c8e280c0d479321d14dae7
Dominique Louis <savagesoftware@hotmail.com>
2022-04-05 13:55:17 +0100

    Attempt at customising the Debug Target dropdown.

---
+++ VS_Meadow_Extension/VS_Meadow_Extension.Shared/MeadowDebugProfileEnumValueGenerator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.Build.Framework.XamlTypes;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
+
+
+namespace Meadow
+{
+    /// <summary>
+    /// Generates dynamic enum values.
+    /// </summary>
+	public class MeadowDebugProfileEnumValueGenerator : IDynamicEnumValuesGenerator
+    {
+        /// <summary>
+        /// Gets whether the dropdown property UI should allow users to type in custom strings
+        /// which will be validated by <see cref="TryCreateEnumValueAsync"/>.
+        /// </summary>
+        public bool AllowCustomValues
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// The list of values for this property that should be displayed to the user as common options.
+        /// It may not be a comprehensive list of all admissible values however.
+        /// </summary>
+        /// <returns>List of sdb devices</returns>
+        /// <seealso cref="AllowCustomValues"/>
+        /// <seealso cref="TryCreateEnumValueAsync"/>
+        public async Task<ICollection<IEnumValue>> GetListedValuesAsync()
+        {
+            await Task.Yield();
+
+            return GetEnumeratorEnumValues();
+        }
+
+        internal static ICollection<IEnumValue> GetEnumeratorEnumValues()
+        {
+            bool hasDevice = true; // Check out device list count here
+
+            if (hasDevice)
+            {
+                var list = new Collection<IEnumValue>();
+                list.Add(new PageEnumValue(new EnumValue() { Name = "COM4", DisplayName = "COM4 (MeadowV1)" }));
+                list.Add(new PageEnumValue(new EnumValue() { Name = "COM5", DisplayName = "COM5 (MeadowV2)" }));
+                list.Add(new PageEnumValue(new EnumValue() { Name = "COM6", DisplayName = "COM6 (MeadowV1)" }));
+
+                return list;
+            }
+            else
+            {
+                return new Collection<IEnumValue>()
+                {
+                    new PageEnumValue(new EnumValue() { Name = "ConnectDevices", DisplayName = "Connect Meadow Devices" })
+                };
+            }
+        }
+
+        /// <summary>
+        /// Tries to find or create an <see cref="IEnumValue"/> based on some user supplied string.
+        /// </summary>
+        /// <param name="userSuppliedValue">The string entered by the user in the property page UI.</param>
+        /// <returns>
+        /// An instance of <see cref="IEnumValue"/> if the <paramref name="userSuppliedValue"/> was successfully used
+        /// to generate or retrieve an appropriate matching <see cref="IEnumValue"/>.
+        /// A task whose result is <c>null</c> otherwise.
+        /// </returns>
+        /// <remarks>
+        /// If <see cref="AllowCustomValues"/> is false, this method is expected to return a task with a <c>null</c> result
+        /// unless the <paramref name="userSuppliedValue"/> matches a value in <see cref="GetListedValuesAsync"/>.
+        /// A new instance of an <see cref="IEnumValue"/> for a value
+        /// that was previously included in <see cref="GetListedValuesAsync"/> may be returned.
+        /// </remarks>
+        public async Task<IEnumValue> TryCreateEnumValueAsync(string userSuppliedValue)
+        {
+            await Task.Yield();
+
+            return new PageEnumValue(new EnumValue() { Name = userSuppliedValue, DisplayName = userSuppliedValue });
+        }
+    }
+}
---
+++ VS_Meadow_Extension/VS_Meadow_Extension.Shared/MeadowDebugProfileEnumValueProvider.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+using Microsoft.Build.Framework.XamlTypes;
+using Microsoft.VisualStudio.ProjectSystem;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
+using Microsoft.VisualStudio.ProjectSystem.Debug;
+using Microsoft.VisualStudio.ProjectSystem.VS.Debug;
+
+namespace Meadow
+{
+    /// <summary>
+    /// Custom property page dynamic enum value provider
+    /// </summary>
+    [ExportDynamicEnumValuesProvider("MeadowDebugProfileEnumValueProvider")]
+    [AppliesTo(Globals.MeadowCapability)]
+    [Export(typeof(IDynamicDebugTargetsGenerator))]
+    [ExportMetadata("Name", "MeadowDebugProfileEnumValueProvider")]
+    public class MeadowDebugProfileEnumValueProvider : ProjectValueDataSourceBase<IReadOnlyList<IEnumValue>>, IDynamicEnumValuesProvider, IDynamicDebugTargetsGenerator
+    {
+        private IReceivableSourceBlock<IProjectVersionedValue<IReadOnlyList<IEnumValue>>> _publicBlock;
+
+        // Represents the link to the launch profiles
+        private IDisposable _launchProfileProviderLink;
+
+        // Represents the link to our source provider
+        private IDisposable _debugProviderLink;
+
+        private NamedIdentity _dataSourceKey = new NamedIdentity();
+        public override NamedIdentity DataSourceKey
+        {
+            get { return _dataSourceKey; }
+        }
+
+        private int _dataSourceVersion;
+		private IDebugProfileLaunchTargetsProvider launchTargetsProvider;
+
+		public override IComparable DataSourceVersion
+        {
+            get { return _dataSourceVersion; }
+        }
+
+
+        public override IReceivableSourceBlock<IProjectVersionedValue<IReadOnlyList<IEnumValue>>> SourceBlock
+        {
+            get
+            {
+                EnsureInitialized();
+                return _publicBlock;
+            }
+        }
+
+        ISourceBlock<IProjectVersionedValue<object>> IProjectValueDataSource.SourceBlock => throw new NotImplementedException();
+
+
+        [ImportingConstructor]
+        public MeadowDebugProfileEnumValueProvider(UnconfiguredProject unconfiguredProject, IDebugProfileLaunchTargetsProvider launchTargetsProvider)
+            : base(unconfiguredProject.Services)
+        {
+            this.launchTargetsProvider = launchTargetsProvider;
+        }
+
+        /// <summary>
+        /// Returns an <see cref="IDynamicEnumValuesGenerator"/> instance prepared to generate dynamic enum values
+        /// given an (optional) set of options.
+        /// </summary>
+        /// <param name="options">
+        /// A set of options set in XAML that helps to customize the behavior of the
+        /// <see cref="IDynamicEnumValuesGenerator "/> instance in some way.
+        /// </param>
+        /// <returns>
+        /// Either a new <see cref="IDynamicEnumValuesGenerator"/> instance
+        /// or an existing one, if the existing one can serve responses based on the given <paramref name="options"/>.
+        /// </returns>
+        public async Task<IDynamicEnumValuesGenerator> GetProviderAsync(IList<NameValuePair> options)
+        {
+            // TODO: Provide your own implementation
+            await Task.Yield();
+
+            return new MeadowDebugProfileEnumValueGenerator();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_launchProfileProviderLink != null)
+                {
+                    _launchProfileProviderLink.Dispose();
+                    _launchProfileProviderLink = null;
+                }
+
+                if (_debugProviderLink != null)
+                {
+                    _debugProviderLink.Dispose();
+                    _debugProviderLink = null;
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
