f648d526c1487ad26ee45495cebbb5644d359131
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-10-22 15:26:11 -0700

    Fix F7Feather comments

--- Meadow.CSharp.Template/MeadowApp.cs
+++ Meadow.CSharp.Template/MeadowApp.cs
@@ -9,65 +9,65 @@ using System.Threading.Tasks;
 
 namespace $safeprojectname$
 {
-	// Change F7FeatherV2 to F7FeatherV1 for V1.x boards
+    // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
     public class MeadowApp : App<F7FeatherV2>
     {
-		RgbPwmLed onboardLed;
+        RgbPwmLed onboardLed;
 
-		public override Task Run()
-		{
-		Console.WriteLine("Run...");
+        public override Task Run()
+        {
+        Console.WriteLine("Run...");
 
-		CycleColors(TimeSpan.FromMilliseconds(1000));
-			return base.Run();
-		}
+        CycleColors(TimeSpan.FromMilliseconds(1000));
+            return base.Run();
+        }
 
-		public override Task Initialize()
-		{
-			Console.WriteLine("Initialize...");
+        public override Task Initialize()
+        {
+            Console.WriteLine("Initialize...");
 
-			onboardLed = new RgbPwmLed(device: Device,
-				redPwmPin: Device.Pins.OnboardLedRed,
-				greenPwmPin: Device.Pins.OnboardLedGreen,
-				bluePwmPin: Device.Pins.OnboardLedBlue,
-				CommonType.CommonAnode);
+            onboardLed = new RgbPwmLed(device: Device,
+                redPwmPin: Device.Pins.OnboardLedRed,
+                greenPwmPin: Device.Pins.OnboardLedGreen,
+                bluePwmPin: Device.Pins.OnboardLedBlue,
+                CommonType.CommonAnode);
 
-			return base.Initialize();
-		}
+            return base.Initialize();
+        }
 
-		void CycleColors(TimeSpan duration)
-		{
-			Console.WriteLine("Cycle colors...");
+        void CycleColors(TimeSpan duration)
+        {
+            Console.WriteLine("Cycle colors...");
 
-			while (true)
-			{
-				ShowColorPulse(Color.Blue, duration);
-				ShowColorPulse(Color.Cyan, duration);
-				ShowColorPulse(Color.Green, duration);
-				ShowColorPulse(Color.GreenYellow, duration);
-				ShowColorPulse(Color.Yellow, duration);
-				ShowColorPulse(Color.Orange, duration);
-				ShowColorPulse(Color.OrangeRed, duration);
-				ShowColorPulse(Color.Red, duration);
-				ShowColorPulse(Color.MediumVioletRed, duration);
-				ShowColorPulse(Color.Purple, duration);
-				ShowColorPulse(Color.Magenta, duration);
-				ShowColorPulse(Color.Pink, duration);
-			}
-		}
+            while (true)
+            {
+                ShowColorPulse(Color.Blue, duration);
+                ShowColorPulse(Color.Cyan, duration);
+                ShowColorPulse(Color.Green, duration);
+                ShowColorPulse(Color.GreenYellow, duration);
+                ShowColorPulse(Color.Yellow, duration);
+                ShowColorPulse(Color.Orange, duration);
+                ShowColorPulse(Color.OrangeRed, duration);
+                ShowColorPulse(Color.Red, duration);
+                ShowColorPulse(Color.MediumVioletRed, duration);
+                ShowColorPulse(Color.Purple, duration);
+                ShowColorPulse(Color.Magenta, duration);
+                ShowColorPulse(Color.Pink, duration);
+            }
+        }
 
-		void ShowColorPulse(Color color, TimeSpan duration)
-		{
-			onboardLed.StartPulse(color, duration / 2);
-			Thread.Sleep(duration);
-			onboardLed.Stop();
-		}
+        void ShowColorPulse(Color color, TimeSpan duration)
+        {
+            onboardLed.StartPulse(color, duration / 2);
+            Thread.Sleep(duration);
+            onboardLed.Stop();
+        }
 
-		void ShowColor(Color color, TimeSpan duration)
-		{
-			onboardLed.SetColor(color);
-			Thread.Sleep(duration);
-			onboardLed.Stop();
-		}
-	}
+        void ShowColor(Color color, TimeSpan duration)
+        {
+            onboardLed.SetColor(color);
+            Thread.Sleep(duration);
+            onboardLed.Stop();
+        }
+    }
 }
