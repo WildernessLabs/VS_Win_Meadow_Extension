6f21de30d16b944d0f04a7e7ce21dfc1ecc0d241
Adrian Stevens <adrian.stevens@gmail.com>
2023-03-05 14:36:42 -0800

    bump versions for RC2-2

--- VS_Meadow_Extension/VS_Meadow_Extension.2019/Properties/AssemblyInfo.cs
+++ VS_Meadow_Extension/VS_Meadow_Extension.2019/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("VS_Meadow_Extension.2019")]
-[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
