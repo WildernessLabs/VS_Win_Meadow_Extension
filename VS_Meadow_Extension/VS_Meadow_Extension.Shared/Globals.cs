4bbde8eb96f89e1046c2a6583dfc07ff81a018e0
Dominique Louis <savagesoftware@gmail.com>
2023-07-23 14:04:42 +0100

    Bump to1.1.2 (#139)
    * Bump to 1.1.2
    
    * Modify CI to handle publishing out of sync with CLI tooling.

--- VS_Meadow_Extension/VS_Meadow_Extension.Shared/Globals.cs
+++ VS_Meadow_Extension/VS_Meadow_Extension.Shared/Globals.cs
@@ -9,7 +9,7 @@ namespace Meadow
 {
     public static class Globals
     {
-        public const string AssemblyVersion = "1.1.1.0";
+        public const string AssemblyVersion = "1.1.2.0";
 
         public const string MeadowCapability = "Meadow";
 
