7fba4febed870fa103d2a4a4317b375c30db8e45
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-12-03 01:12:03 -0800

    Update CoreCompute.Template

--- Meadow.CSharp.CoreCompute.Template/MeadowApp.cs
+++ Meadow.CSharp.CoreCompute.Template/MeadowApp.cs
@@ -9,23 +9,23 @@ using System.Threading.Tasks;
 
 namespace $safeprojectname$
 {
-	// Change F7CoreComputeV2 to F7FeatherV2 (or F7FeatherV1) for Feather boards
-	public class MeadowApp : App<F7CoreComputeV2>
-	{
-		public override Task Run()
-		{
-			Resolver.Log.Info("Run...");
+    // Change F7CoreComputeV2 to F7FeatherV2 (or F7FeatherV1) for Feather boards
+    public class MeadowApp : App<F7CoreComputeV2>
+    {
+        public override Task Initialize()
+        {
+            Resolver.Log.Info("Initialize...");
 
-			Resolver.Log.Info("Hello, Meadow Core-Compute!");
+            return base.Initialize();
+        }
 
-			return base.Run();
-		}
+        public override Task Run()
+        {
+            Resolver.Log.Info("Run...");
 
-		public override Task Initialize()
-		{
-			Resolver.Log.Info("Initialize...");
+            Resolver.Log.Info("Hello, Meadow Core-Compute!");
 
-			return base.Initialize();
-		}
-	}
+            return base.Run();
+        }
+    }
 }
