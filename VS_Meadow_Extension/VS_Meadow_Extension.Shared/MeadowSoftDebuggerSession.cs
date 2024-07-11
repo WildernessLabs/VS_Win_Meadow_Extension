8cf40697eae31ed54a925ef57cc24c2c1f02e34a
CartBlanche <savagesoftware@gmail.com>
2024-07-11 13:39:27 +0100

    Refactor to remove duplicate hooking up of DeviceMessages and implment standard dispose pattern.

--- VS_Meadow_Extension/VS_Meadow_Extension.Shared/MeadowSoftDebuggerSession.cs
+++ VS_Meadow_Extension/VS_Meadow_Extension.Shared/MeadowSoftDebuggerSession.cs
@@ -1,66 +1,117 @@
-﻿using System.Threading;
-
+﻿using Meadow.Hcom;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.Shell;
 using Mono.Debugging.Client;
 using Mono.Debugging.Soft;
-using Meadow.Hcom;
 using System;
-using Microsoft.Build.Utilities;
-using Microsoft.Extensions.Logging;
+using System.Threading;
 
 namespace Meadow
 {
-    class MeadowSoftDebuggerSession : SoftDebuggerSession
-    {
-        CancellationTokenSource meadowDebugCancelTokenSource;
-        DebuggingServer meadowDebugServer;
-        IMeadowConnection meadowConnection;
-        private ILogger logger;
+	class MeadowSoftDebuggerSession : SoftDebuggerSession
+	{
+		CancellationTokenSource meadowDebugCancelTokenSource;
+		DebuggingServer meadowDebugServer;
+		IMeadowConnection meadowConnection;
+		private ILogger logger;
 
-        public MeadowSoftDebuggerSession(IMeadowConnection meadowConnection, ILogger deployOutputLogger)
-        {
-            this.meadowConnection = meadowConnection;
-            this.logger = deployOutputLogger;
-            meadowDebugCancelTokenSource = new CancellationTokenSource();
-        }
+		public MeadowSoftDebuggerSession(IMeadowConnection meadowConnection, ILogger deployOutputLogger)
+		{
+			this.meadowConnection = meadowConnection;
+			this.logger = deployOutputLogger;
+			meadowDebugCancelTokenSource = new CancellationTokenSource();
+		}
 
-        protected override async void OnRun(DebuggerStartInfo startInfo)
-        {
-            var meadowStartInfo = startInfo as SoftDebuggerStartInfo;
-            var connectArgs = meadowStartInfo.StartArgs as SoftDebuggerConnectArgs;
-            var port = connectArgs?.DebugPort ?? 0;
+		protected override async void OnRun(DebuggerStartInfo startInfo)
+		{
+			try
+			{
+				if (startInfo is SoftDebuggerStartInfo meadowStartInfo)
+				{
+					if (meadowStartInfo.StartArgs is SoftDebuggerConnectArgs connectArgs)
+					{
+						if (connectArgs.DebugPort == 0)
+						{
+							logger.LogError("Invalid debug port specified.");
+							return;
+						}
 
-            meadowDebugServer = await meadowConnection.StartDebuggingSession(port, logger, meadowDebugCancelTokenSource.Token);
+						var port = connectArgs?.DebugPort ?? 0;
 
-            meadowConnection.DeviceMessageReceived += MeadowConnection_DeviceMessageReceived;
+						await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
 
-            base.OnRun(startInfo);
-        }
+						meadowDebugServer = await meadowConnection.StartDebuggingSession(port, logger, meadowDebugCancelTokenSource.Token);
+					}
+					else
+					{
+						logger.LogError("Invalid debug arguments specified.");
+						return;
+					}
+				}
+			}
+			catch(Exception ex)
+			{
+				logger.LogError($"Unable to Debug due to:{ex.Message}");
+			}
+			finally
+			{
+				base.OnRun(startInfo);
+			}
+		}
 
-        private void MeadowConnection_DeviceMessageReceived(object sender, (string message, string source) e)
-        {
-            logger.LogInformation($"{e.message}");
-        }
+		protected override async void OnExit()
+		{
+			if (!meadowDebugCancelTokenSource.IsCancellationRequested)
+				meadowDebugCancelTokenSource?.Cancel();
 
-        protected override async void OnExit()
-        {
-            if (!meadowDebugCancelTokenSource.IsCancellationRequested)
-                meadowDebugCancelTokenSource?.Cancel();
+			if (meadowDebugServer != null)
+			{
+				await meadowDebugServer.StopListening();
+				meadowDebugServer.Dispose();
+				meadowDebugServer = null;
+			}
 
-            if (meadowDebugServer != null)
-            {
-                await meadowDebugServer.StopListening();
-                meadowDebugServer.Dispose();
-                meadowDebugServer = null;
-            }
+			if (meadowConnection != null)
+			{
+				meadowConnection.Dispose();
+				meadowConnection = null;
+			}
 
-            if (meadowConnection != null)
-            {
-                meadowConnection.DeviceMessageReceived -= MeadowConnection_DeviceMessageReceived;
-                meadowConnection.Dispose();
-                meadowConnection = null;
-            }
+			base.OnExit();
+		}
 
-            base.OnExit();
-        }
-    }
+		public override void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+
+			base.Dispose();
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				if (meadowDebugCancelTokenSource != null
+				&& !meadowDebugCancelTokenSource.IsCancellationRequested)
+				{
+					meadowDebugCancelTokenSource.Cancel();
+					meadowDebugCancelTokenSource.Dispose();
+					meadowDebugCancelTokenSource = null;
+				}
+
+				if (meadowDebugServer != null)
+				{
+					meadowDebugServer.Dispose();
+					meadowDebugServer = null;
+				}
+
+				if (meadowConnection != null)
+				{
+					meadowConnection.Dispose();
+					meadowConnection = null;
+				}
+			}
+		}
+	}
 }
