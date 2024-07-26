64e4dbc856fdd1c64a22112101be26ccab738844
Adrian Stevens <adrian.stevens@gmail.com>
2024-07-25 21:14:53 -0700

    Bump version and add file logger

--- VS_Meadow_Extension/VS_Meadow_Extension.Shared/Globals.cs
+++ VS_Meadow_Extension/VS_Meadow_Extension.Shared/Globals.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 
 namespace Meadow
 {
     public static class Globals
     {
-        public const string AssemblyVersion = "2.0.0.6";
+        public const string AssemblyVersion = "2.0.0.7";
 
         public const string MeadowCapability = "Meadow";
 
@@ -14,5 +15,7 @@ namespace Meadow
         public static string SettingsFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "WildernessLabs", ".meadowsettings");
         public static string ExtensionLogFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "WildernessLabs", "extension.log");
         public static string FirmwareDownloadsFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "WildernessLabs", "Firmware");
+
+        public static CancellationTokenSource DebugDeployTokenSource { get; set; }
     }
 }
