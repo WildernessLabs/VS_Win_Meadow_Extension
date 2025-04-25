1c859fd2eddfc2bfcf6d75a8f8f45d883a9ff916
Adrian Stevens <adrian.stevens@gmail.com>
2025-04-24 17:02:59 -0700

    Version bump

--- VS_Meadow_Extension/VS_Meadow_Extension.Shared/Globals.cs
+++ VS_Meadow_Extension/VS_Meadow_Extension.Shared/Globals.cs
@@ -6,7 +6,7 @@ namespace Meadow
 {
     public static class Globals
     {
-        public const string AssemblyVersion = "2.0.0.15";
+        public const string AssemblyVersion = "2.1.0.0";
 
         public const string MeadowCapability = "Meadow";
 
